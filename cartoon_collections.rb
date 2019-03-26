0aa3fb1378c91c47c5fc33ef00cc78b816cf946a
Spencer Lindemuth <lindemut@gmail.com>
2019-03-26 22:22:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,8 +23,8 @@ end
 def find_the_cheese(food)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food.any? do |x| 
-    cheese_types.include? x 
+  food.any? do |x|
+    cheese_types.include? x
   end
 
 end
