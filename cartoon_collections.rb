c30d7b571a5d0758a5b25985494c560e1dca02e4
Spencer Lindemuth <lindemut@gmail.com>
2019-03-26 22:11:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarfs)# code an argument here
   # Your code here
-  dwarfs.each do |dwarf , x|
+  dwarfs.each_with_index do |dwarf , x|
     puts "#{x + 1}. #{dwarf}"
   end
 end
